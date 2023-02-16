904df0ad27dc660f4ce8a02c792577aa53b9d551
jplumail <jeanplumail76@gmail.com>
2023-02-16 12:52:12 +0100

    seed cuda

--- spfluo/picking/modules/utils.py
+++ spfluo/picking/modules/utils.py
@@ -14,6 +14,7 @@ def seed_all(seed_numpy: bool=True) -> None:
     """
     random.seed(0)
     torch.manual_seed(0)
+    torch.cuda.manual_seed_all(0)
     if seed_numpy:
         np.random.seed(0)
 
