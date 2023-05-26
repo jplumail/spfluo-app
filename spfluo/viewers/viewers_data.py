bae2cc19f8b2cc32b2f6be69a57481f9f825ee6a
Jean Plumail <jplumail@unistra.fr>
2023-05-26 16:18:55 +0200

    adds a napari viewer for Image objects It includes FluoImage, Particle, AverageParticle

--- spfluo/viewers/viewers_data.py
+++ spfluo/viewers/viewers_data.py
@@ -2,7 +2,7 @@ from spfluo import objects as fluoobj
 from spfluo import Plugin
 from spfluo.constants import VISUALISATION_MODULE
 from spfluo.convert import save_coordinates3D
-from spfluo.objects.data import FluoImage, SetOfCoordinates3D
+from spfluo.objects.data import FluoImage, Image, SetOfCoordinates3D
 from spfluo.protocols.protocol_base import ProtFluoBase
 
 from pyworkflow.viewer import Viewer, View, DESKTOP_TKINTER
@@ -21,7 +21,8 @@ class NapariDataViewer(Viewer):
     """
     _environments = [DESKTOP_TKINTER]
     _targets = [
-        fluoobj.SetOfCoordinates3D
+        fluoobj.SetOfCoordinates3D,
+        fluoobj.Image,
     ]
 
     def __init__(self, **kwargs):
@@ -33,8 +34,28 @@ class NapariDataViewer(Viewer):
 
         if issubclass(cls, fluoobj.SetOfCoordinates3D):
             self._views.append(SetOfCoordinates3DView(self._tkRoot, obj, self.protocol))
+        elif issubclass(cls, fluoobj.Image):
+            self._views.append(ImageView(obj))
         
         return self._views
+
+
+###########
+## Image ##
+###########
+
+class ImageView(View):
+    def __init__(self, image: fluoobj.Image):
+        self.image = image
+    
+    def show(self):
+        self.proc = threading.Thread(target=self.lanchNapariForImage, args=(self.image,))
+        self.proc.start()
+    
+    def lanchNapariForImage(self, im: fluoobj.Image):
+        path = im.getFileName()
+        fullProgram = Plugin.getFullProgram(Plugin.getNapariProgram())
+        runJob(None, fullProgram, path, env=Plugin.getEnviron())
 
 
 ########################
