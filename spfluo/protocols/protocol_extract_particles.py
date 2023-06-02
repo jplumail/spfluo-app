5f96dd6b8d78abcdf0b04b2403d163cc2daf74ff
Jean Plumail <jplumail@unistra.fr>
2023-06-02 09:45:52 +0200

    adds a subpixel option in extract particle, closes #29

--- spfluo/protocols/protocol_extract_particles.py
+++ spfluo/protocols/protocol_extract_particles.py
@@ -6,7 +6,7 @@ from pyworkflow.protocol import Protocol, Form, params
 
 import os
 import numpy as np
-from scipy.ndimage import affine_transform
+from scipy.ndimage import affine_transform, shift
 from aicsimageio.aics_image import AICSImage
 
 
@@ -26,6 +26,13 @@ class ProtSPFluoExtractParticles(Protocol, ProtFluoBase):
             label='coordinates you want to extract',
             important=True
         )
+        form.addParam(
+            'subpixel',
+            params.BooleanParam,
+            label='Subpixel precision?',
+            default=False,
+            expertLevel=params.LEVEL_ADVANCED,
+        )
     
     def _insertAllSteps(self):
         self._insertFunctionStep(self.createOutputStep)
@@ -38,21 +45,30 @@ class ProtSPFluoExtractParticles(Protocol, ProtFluoBase):
         for im in fluoimages.iterItems():
             im: FluoImage
             for coord_im in coords.iterCoordinates(im):
-                extracted_particle = self.extract_particle(im, coord_im, box_size)
+                extracted_particle = self.extract_particle(im, coord_im, box_size, subpixel=self.subpixel.get())
                 particles.append(extracted_particle)
         particles.write()
         
         self._defineOutputs(**{self.OUTPUT_NAME: particles})
     
-    def extract_particle(self, im: FluoImage, coord: Coordinate3D, box_size: int) -> Particle:
+    def extract_particle(self, im: FluoImage, coord: Coordinate3D, box_size: int, subpixel: bool=False) -> Particle:
         mat = coord.getMatrix()
         mat[:3, 3] -= float(box_size) / 2
+        mat[:3, :3] = np.eye(3)
         image_data = im.getData()
         C = im.getNumChannels()
         particle_data = np.empty((1,C)+(box_size,)*3, dtype=image_data.dtype)
+        if not subpixel:
+            top_left_corner = np.rint(mat[:3,3]).astype(int)
+            bottom_right_corner = top_left_corner + box_size
+            xmin, ymin, zmin = top_left_corner
+            xmax, ymax, zmax = bottom_right_corner
         for c in range(C):
             im_array_c = image_data[0, c] # T=0,C=c in AICS model
-            particle_data[0, c] = affine_transform(im_array_c, mat, output_shape=(box_size,)*3)
+            if subpixel:
+                particle_data[0, c] = affine_transform(im_array_c, mat, output_shape=(box_size,)*3)
+            else:
+                particle_data[0, c] = im_array_c[xmin:xmax, ymin:ymax, zmin:zmax]
 
         ext = os.path.splitext(im.getFileName())[1]
         coord_str = "-".join([f"{x:.2f}" for x in coord.getPosition()])
