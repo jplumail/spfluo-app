ba0409b5f5db582509c7e9d7ab5c558e08dfd67e
jplumail <jeanplumail76@gmail.com>
2023-06-28 09:28:38 +0200

    remove unused import

--- spfluo/picking/modules/pretraining/generate/__init__.py
+++ spfluo/picking/modules/pretraining/generate/__init__.py
@@ -1,5 +1,4 @@
 from .config import DataGenerationConfig
 from .data_generator import DataGenerator
-from .setup import setup
 
-__all__ = (DataGenerationConfig, DataGenerator, setup)
+__all__ = (DataGenerationConfig, DataGenerator)
