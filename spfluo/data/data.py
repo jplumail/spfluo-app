0de786b5a7e140e64078e1be2a7a30fdb97e2d1d
Jean Plumail <jplumail@unistra.fr>
2024-01-30 09:15:13 +0100

    move from github to seafile for data hosting remove pooch from deps

--- spfluo/data/data.py
+++ spfluo/data/data.py
@@ -1,69 +1,89 @@
 import csv
-import importlib.metadata
+import hashlib
+import io
+import tempfile
+import zipfile
+from importlib.resources import as_file, files
 from pathlib import Path
 from typing import Dict
 
 import numpy as np
-import pkg_resources
-import pooch
+import requests
 import tifffile
 
-version = importlib.metadata.version("spfluo")
 
-POOCH = pooch.create(
-    # Use the default cache folder for the operating system
-    path=pooch.os_cache("spfluo"),
-    # The remote data is on Github
-    base_url="https://github.com/jplumail/spfluo/raw/{version}/spfluo/data/",
-    version=version,
-    # If this is a development version, get the data from the "main" branch
-    version_dev="main",
-    registry=None,
-)
+def _download_data(d: Path):
+    url = "https://seafile.unistra.fr/f/ce50bfac098a45c38c99/?dl=1"
+    response = requests.get(url)
 
-registry_file = pkg_resources.resource_stream("spfluo", "data/registry.txt")
-POOCH.load_registry(registry_file)
+    if response.status_code == 200:
+        # Write the content of the response to a file
+        with tempfile.TemporaryFile() as fp:
+            fp.write(response.content)
+            fp.seek(0)
+            with zipfile.ZipFile(io.BytesIO(fp.read()), "r") as zip_ref:
+                zip_ref.extractall(d)
+            return True
+    return False
 
 
-def _fetch_dataset(dataset_dir) -> Dict[str, np.ndarray]:
-    root_dir = Path(f"generated/{dataset_dir}")
-    poses_path = Path(POOCH.fetch((root_dir / "poses.csv").as_posix()))
-    content = csv.reader(poses_path.read_text().split("\n"))
-    next(content)  # skip header
-    data = {}
-    for row in content:
-        if len(row) == 7:
-            particle_path = POOCH.fetch((root_dir / row[0]).as_posix())
-            data[row[0]] = {
-                "array": tifffile.imread(particle_path),
-                "rot": np.array(row[1:4], dtype=float),
-                "trans": np.array(row[4:7], dtype=float),
-            }
+def _check_data(d: Path):
+    registry = d / "registry.txt"
+    for line in registry.read_text().strip().split("\n"):
+        path, hash = line.split(" ")
+        path = d / path
+        if (not path.exists()) or hashlib.sha256(path.read_bytes()).hexdigest() != hash:
+            return False
+    return True
 
-    N = len(data)
-    p0 = next(iter(data))
-    D = data[p0]["array"].shape[0]
-    dtype = data[p0]["array"].dtype
-    volumes = np.empty((N, D, D, D), dtype=dtype)
-    poses = np.empty((len(data), 6))
-    for i, k in enumerate(data):
-        p = data[k]
-        rot = p["rot"]
-        trans = p["trans"]
-        poses[i, :3] = rot
-        poses[i, 3:] = trans
-        volumes[i] = p["array"]
 
-    psf = tifffile.imread(POOCH.fetch((root_dir / "psf.tiff").as_posix()))
-    gt = tifffile.imread(POOCH.fetch((root_dir / "gt.tiff").as_posix()))
+def _fetch_dataset(dataset_dir: str) -> Dict[str, np.ndarray]:
+    # Download if necessary
+    with as_file(files("spfluo").joinpath("data")) as data_dir:
+        if not _check_data(data_dir):
+            _download_data(data_dir)
+            if not _check_data(data_dir):
+                raise RuntimeError("Download failed")
 
-    return {
-        "volumes": volumes,
-        "poses": poses,
-        "psf": psf,
-        "gt": gt,
-        "rootdir": poses_path.parent.absolute(),
-    }
+        # parse data
+        root_dir = data_dir / "generated" / dataset_dir
+        poses_path = root_dir / "poses.csv"
+        content = csv.reader(poses_path.read_text().split("\n"))
+        next(content)  # skip header
+        data = {}
+        for row in content:
+            if len(row) == 7:
+                particle_path = (root_dir / row[0]).as_posix()
+                data[row[0]] = {
+                    "array": tifffile.imread(particle_path),
+                    "rot": np.array(row[1:4], dtype=float),
+                    "trans": np.array(row[4:7], dtype=float),
+                }
+
+        N = len(data)
+        p0 = next(iter(data))
+        D = data[p0]["array"].shape[0]
+        dtype = data[p0]["array"].dtype
+        volumes = np.empty((N, D, D, D), dtype=dtype)
+        poses = np.empty((len(data), 6))
+        for i, k in enumerate(data):
+            p = data[k]
+            rot = p["rot"]
+            trans = p["trans"]
+            poses[i, :3] = rot
+            poses[i, 3:] = trans
+            volumes[i] = p["array"]
+
+        psf = tifffile.imread((root_dir / "psf.tiff").as_posix())
+        gt = tifffile.imread((root_dir / "gt.tiff").as_posix())
+
+        return {
+            "volumes": volumes,
+            "poses": poses,
+            "psf": psf,
+            "gt": gt,
+            "rootdir": poses_path.parent.absolute(),
+        }
 
 
 def generated_isotropic():
