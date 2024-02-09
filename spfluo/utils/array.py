0afc137c902ae0cb8de9da3d04087c2ea420a882
Jean Plumail <jplumail@unistra.fr>
2024-02-09 16:23:17 +0100

    cpu_only_compatibility to numpy_only_compatibility

--- spfluo/utils/array.py
+++ spfluo/utils/array.py
@@ -27,44 +27,44 @@ else:
     cupy = None
 
 Array: TypeAlias = ArrayAPIArray
-
 if TYPE_CHECKING:
-    import numpy.array_api as array_api
+    from spfluo._vendored.array_api_compat.common._helpers import array_api_module
 
 
-def cpu_only_compatibility(cpu_func):
+def numpy_only_compatibility(numpy_func):
     """
-    Apply this decorator to cpu only functions to make them compliant with the array-api
-    cpu_func: Callable
+    Apply this decorator to numpy only functions to make them compliant
+    with the array-api
+    numpy_func: Callable
         signature (*args, **kwargs) -> array like object
     """
 
-    @functools.wraps(cpu_func)
+    @functools.wraps(numpy_func)
     def func(*args, **kwargs) -> Array:
         array_args = list(filter(is_array_api_obj, args))
         array_kwargs = list(filter(is_array_api_obj, kwargs.values()))
         xp = array_namespace(*array_args, *array_kwargs)
-        cpu_args = []
+        numpy_args = []
         for arg in args:
             arg_ = arg
             if is_array_api_obj(arg):
                 arg_ = xp.asarray(arg)
                 arg_ = xp.to_device(arg_, "cpu")
-            cpu_args.append(arg_)
-        cpu_kwargs = {}
+            numpy_args.append(arg_)
+        numpy_kwargs = {}
         for k, arg in kwargs.items():
             arg_ = arg
             if is_array_api_obj(arg):
                 arg_ = xp.asarray(arg)
                 arg_ = xp.to_device(arg_, "cpu")
-            cpu_kwargs[k] = arg_
+            numpy_kwargs[k] = arg_
 
         devices = set(map(xp.device, array_args + array_kwargs))
         if len(devices) != 1:
             raise TypeError(f"Multiple devices found in args: {devices}")
         (device,) = devices
 
-        return xp.asarray(cpu_func(*cpu_args, **cpu_kwargs), device=device)
+        return xp.asarray(numpy_func(*numpy_args, **numpy_kwargs), device=device)
 
     return func
 
@@ -72,4 +72,11 @@ def cpu_only_compatibility(cpu_func):
 __all__ = [Array, array_namespace, is_array_api_obj, to_device, *libs]
 
 if TYPE_CHECKING:
-    __all__ = [Array, array_namespace, is_array_api_obj, to_device, *libs, array_api]
+    __all__ = [
+        Array,
+        array_namespace,
+        is_array_api_obj,
+        to_device,
+        *libs,
+        array_api_module,
+    ]
