d2b51a15486df91cd9cb6a10524bc9d74bc3e3a8
Jean Plumail <jplumail@unistra.fr>
2024-05-22 15:05:40 +0200

    updates symmetrize_particle.py, no more pytorch dep

--- spfluo/utils/symmetrize_particle.py
+++ spfluo/utils/symmetrize_particle.py
@@ -1,26 +1,36 @@
-import torch
+from typing import TYPE_CHECKING
 
 from spfluo.refinement.refinement import reconstruction_L2
+from spfluo.utils.array import array_namespace, get_device
 from spfluo.utils.transform import symmetrize_poses
 from spfluo.utils.volume import center_of_mass
 
+if TYPE_CHECKING:
+    from spfluo.utils.array import Array
+
 
 def symmetrize(
-    particle: torch.Tensor,
+    particle: "Array",
     center: tuple[float, float],
     symmetry: int,
-    psf: torch.Tensor,
-    lambda_: torch.Tensor,
+    psf: "Array",
+    lambda_: "Array",
 ):
-    tensor_kwargs = dict(dtype=float, device=particle.device)
+    xp = array_namespace(particle, psf, lambda_)
     zc, _, _ = center_of_mass(particle)
     pose_syms = symmetrize_poses(
-        torch.as_tensor(
+        xp.asarray(
             [0, 0, 0, -(particle.shape[0] / 2 - zc), center[0], center[1]],
-            **tensor_kwargs,
+            device=get_device(particle),
+            dtype=xp.float64,
         ),
         symmetry=symmetry,
     )
     return reconstruction_L2(
-        particle[None], psf, pose_syms[:, None], lambda_, symmetry=True
-    )
+        xp.astype(particle[None, None], xp.float32),
+        xp.astype(psf[None], xp.float32),
+        pose_syms[:, None],
+        lambda_,
+        symmetry=True,
+        device=get_device(particle),
+    )[0]
