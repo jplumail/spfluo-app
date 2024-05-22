4bf6c86378806109e0f410ad416840e7509b7cdf
Jean Plumail <jplumail@unistra.fr>
2024-05-22 12:29:11 +0200

    loading.py, making sure data is read in correct order when reading from OME-TIFF

--- spfluo/utils/loading.py
+++ spfluo/utils/loading.py
@@ -1,8 +1,11 @@
+# Some functions were taken from aicsimageio source code
+
 import csv
 import math
 import os
 import pickle
-from typing import Dict, Optional, Tuple
+from collections import Counter
+from typing import Any, Dict, Optional, Tuple
 
 import imageio
 import numpy as np
@@ -13,6 +16,293 @@ from ome_types.model.simple_types import UnitsLength
 import spfluo
 from spfluo.utils.volume import interpolate_to_size
 from spfluo.utils.volume import resample as _resample
+
+
+class DimensionNames:
+    Time = "T"
+    Channel = "C"
+    SpatialZ = "Z"
+    SpatialY = "Y"
+    SpatialX = "X"
+    Samples = "S"
+    MosaicTile = "M"
+
+
+DEFAULT_DIMENSION_ORDER_LIST = [
+    DimensionNames.Time,
+    DimensionNames.Channel,
+    DimensionNames.SpatialZ,
+    DimensionNames.SpatialY,
+    DimensionNames.SpatialX,
+]
+
+DEFAULT_DIMENSION_ORDER = "".join(DEFAULT_DIMENSION_ORDER_LIST)
+
+DEFAULT_DIMENSION_ORDER_LIST_WITH_SAMPLES = DEFAULT_DIMENSION_ORDER_LIST + [
+    DimensionNames.Samples
+]
+
+DEFAULT_DIMENSION_ORDER_WITH_SAMPLES = "".join(
+    DEFAULT_DIMENSION_ORDER_LIST_WITH_SAMPLES
+)
+
+
+def transpose_to_dims(
+    data: np.ndarray,
+    given_dims: str,
+    return_dims: str,
+) -> np.ndarray:
+    """
+    This shuffles the data dimensions from given_dims to return_dims. Each dimension
+    must be present in given_dims must be used in return_dims
+
+    Parameters
+    ----------
+    data: types.ArrayLike
+        Either a dask array or numpy.ndarray of arbitrary shape but with the dimensions
+        specified in given_dims
+    given_dims: str
+        The dimension ordering of data, "CZYX", "VBTCXZY" etc
+    return_dims: str
+        The dimension ordering of the return data
+
+    Returns
+    -------
+    data: types.ArrayLike
+        The data with the specified dimension ordering.
+
+    Raises
+    ------
+    ConflictingArgumentsError
+        given_dims and return_dims are incompatible.
+    """
+    # Use a counter to track that the contents are composed of the same letters
+    # and that no letter is repeated
+    if (
+        Counter(given_dims) != Counter(return_dims)
+        or max(Counter(given_dims).values()) > 1
+    ):
+        raise ValueError(
+            f"given_dims={given_dims} and return_dims={return_dims} are incompatible."
+        )
+
+    # Resort the data into return_dims order
+    match_map = {dim: given_dims.find(dim) for dim in given_dims}
+    transposer = []
+    for dim in return_dims:
+        transposer.append(match_map[dim])
+    data = data.transpose(transposer)
+
+    return data
+
+
+def reduce_to_slice(L: list | tuple) -> int | list | slice | tuple:
+    # if the list only has one element, then just use it
+    if len(L) == 1:
+        return slice(L[0], L[0] + 1)
+    # if the list has at least 2 elements we can check for sliceable
+    # it is convertable to a slice if the step size between each
+    # consecutive pair of elements is equal and positive
+    # 1. get all the deltas in a list:
+    steps = [(L[i + 1] - L[i]) for i in range(len(L) - 1)]
+    # 2. check if all the deltas are equal and positive
+    if steps[0] > 0 and steps.count(steps[0]) == len(steps):
+        return slice(min(L), max(L) + 1, steps[0])
+    # if we can't convert to a slice, then just return the list unmodified
+    return L
+
+
+def reshape_data(
+    data: np.ndarray, given_dims: str, return_dims: str, **kwargs: Any
+) -> np.ndarray:
+    """
+    Reshape the data into return_dims, pad missing dimensions, and prune extra
+    dimensions. Warns the user to use the base reader if the depth of the Dimension
+    being removed is not 1.
+
+    Parameters
+    ----------
+    data: types.ArrayLike
+        Either a dask array or numpy.ndarray of arbitrary shape but with the dimensions
+        specified in given_dims
+    given_dims: str
+        The dimension ordering of data, "CZYX", "VBTCXZY" etc
+    return_dims: str
+        The dimension ordering of the return data
+    kwargs:
+        * C=1 => desired specific channel, if C in the input data has depth 3 then C=1
+          returns the 2nd slice (0 indexed)
+        * Z=10 => desired specific channel, if Z in the input data has depth 20 then
+          Z=10 returns the 11th slice
+        * T=[0, 1] => desired specific timepoints, if T in the input data has depth 100
+          then T=[0, 1] returns the 1st and 2nd slice (0 indexed)
+        * T=(0, 1) => desired specific timepoints, if T in the input data has depth 100
+          then T=(0, 1) returns the 1st and 2nd slice (0 indexed)
+        * T=(0, -1) => desired specific timepoints, if T in the input data has depth 100
+          then T=(0, -1) returns the first and last slice
+        * T=range(10) => desired specific timepoints, if T in the input data has depth
+          100 then T=range(10) returns the first ten slices
+        * T=slice(0, -1, 5) => desired specific timepoints, T=slice(0, -1, 5) returns
+          every fifth timepoint
+
+    Returns
+    -------
+    data: types.ArrayLike
+        The data with the specified dimension ordering.
+
+    Raises
+    ------
+    ConflictingArgumentsError
+        Missing dimension in return dims when using range, slice, or multi-index
+        dimension selection for the requested dimension.
+
+    IndexError
+        Requested dimension index not present in data.
+
+    Examples
+    --------
+    Specific index selection
+
+    >>> data = np.random.rand((10, 100, 100))
+    ... z1 = reshape_data(data, "ZYX", "YX", Z=1)
+
+    List of index selection
+
+    >>> data = np.random.rand((10, 100, 100))
+    ... first_and_second = reshape_data(data, "ZYX", "YX", Z=[0, 1])
+
+    Tuple of index selection
+
+    >>> data = np.random.rand((10, 100, 100))
+    ... first_and_last = reshape_data(data, "ZYX", "YX", Z=(0, -1))
+
+    Range of index selection
+
+    >>> data = np.random.rand((10, 100, 100))
+    ... first_three = reshape_data(data, "ZYX", "YX", Z=range(3))
+
+    Slice selection
+
+    >>> data = np.random.rand((10, 100, 100))
+    ... every_other = reshape_data(data, "ZYX", "YX", Z=slice(0, -1, 2))
+
+    Empty dimension expansion
+
+    >>> data = np.random.rand((10, 100, 100))
+    ... with_time = reshape_data(data, "ZYX", "TZYX")
+
+    Dimension order shuffle
+
+    >>> data = np.random.rand((10, 100, 100))
+    ... as_zx_base = reshape_data(data, "ZYX", "YZX")
+
+    Selections, empty dimension expansions, and dimension order shuffle
+
+    >>> data = np.random.rand((10, 100, 100))
+    ... example = reshape_data(data, "CYX", "BSTCZYX", C=slice(0, -1, 3))
+    """
+    # Check for parameter conflicts
+    for dim in given_dims:
+        # return_dims='CZYX' and iterable dimensions 'T=range(10)'
+        # Dimension is in kwargs
+        # Dimension is an iterable
+        # Dimension is not in return dimensions
+        if (
+            isinstance(kwargs.get(dim), (list, tuple, range, slice))
+            and dim not in return_dims
+        ):
+            raise ValueError(
+                f"When selecting a multiple dimension indices, the specified "
+                f"dimension must be provided in return_dims. "
+                f"return_dims={return_dims}, dimension {dim} = {kwargs.get(dim)}"
+            )
+
+    # Process each dimension available
+    new_dims = given_dims
+    dim_specs = []
+    for dim in given_dims:
+        # Store index of the dim as it is in given data
+        dim_index = given_dims.index(dim)
+
+        # Handle dim in return_dims which means that it is
+        # an iterable or None selection
+        if dim in return_dims:
+            # Specific iterable requested
+            if dim in kwargs:
+                # Actual dim specification
+                # The specification provided for this dimension in the kwargs
+                dim_spec = kwargs.get(dim)
+                display_dim_spec = dim_spec
+
+                if isinstance(dim_spec, int):
+                    dim_spec = slice(dim_spec, dim_spec + 1)
+
+                # Convert operator to standard list or slice
+                # dask.Array and numpy.ndarray both natively support
+                # List[int] and slices being passed to getitem so no need to cast them
+                # to anything different
+                if isinstance(dim_spec, (tuple, range)):
+                    dim_spec = list(dim_spec)
+
+                # Get the largest absolute value index in the list using min and max
+                if isinstance(dim_spec, list):
+                    check_selection_max = max([abs(min(dim_spec)), max(dim_spec)])
+                    # try to convert to slice if possible
+                    dim_spec = reduce_to_slice(dim_spec)
+
+                # Get the largest absolute value index from start and stop of slice
+                if isinstance(dim_spec, slice):
+                    check_selection_max = max([abs(dim_spec.stop), abs(dim_spec.start)])
+            else:
+                # Nothing was requested from this dimension
+                dim_spec = slice(None, None, None)
+                display_dim_spec = dim_spec
+
+                # No op means that it doesn't matter how much data is in this dimension
+                check_selection_max = 0
+
+        # Not in return_dims means that it is a fixed integer selection
+        else:
+            if dim in kwargs:
+                # Integer requested
+                dim_spec = kwargs.get(dim)
+                display_dim_spec = dim_spec
+
+                # Check that integer
+                check_selection_max = dim_spec
+            else:
+                dim_spec = 0
+                display_dim_spec = dim_spec
+                check_selection_max = 0
+
+            # Remove dim from new dims as it is fixed size
+            new_dims = new_dims.replace(dim, "")
+
+        # Check that fixed integer request isn't outside of request
+        if check_selection_max > data.shape[dim_index]:
+            raise IndexError(
+                f"Dimension specified with {dim}={display_dim_spec} "
+                f"but Dimension shape is {data.shape[dim_index]}."
+            )
+
+        # All checks and operations passed, append dim operation to getitem ops
+        dim_specs.append(dim_spec)
+
+    # Run getitems
+    data = data[tuple(dim_specs)]
+
+    # Add empty dims where dimensions were requested but data doesn't exist
+    # Add dimensions to new dims where empty dims are added
+    for i, dim in enumerate(return_dims):
+        # This dimension wasn't processed
+        if dim not in given_dims:
+            new_dims = f"{new_dims[:i]}{dim}{new_dims[i:]}"
+            data = data.reshape(*data.shape[:i], 1, *data.shape[i:])
+
+    # Any extra dimensions have been removed, only a problem if the depth is > 1
+    return transpose_to_dims(
+        data, given_dims=new_dims, return_dims=return_dims
+    )  # don't pass kwargs or 2 copies
 
 
 def _get_dims_from_ome(ome: OME, scene_index: int) -> list[str]:
@@ -87,18 +377,110 @@ def _guess_ome_dim_order(
     return dims
 
 
+def _expand_dims_to_match_ome(
+    image_data: np.ndarray,
+    ome: OME,
+    dims: list[str],
+    scene_index: int,
+) -> np.ndarray:
+    # Expand image_data for empty dimensions
+    ome_shape = []
+
+    # need to correct channel count if this is a RGB image
+    n_samples = ome.images[scene_index].pixels.channels[0].samples_per_pixel
+    has_multiple_samples = n_samples is not None and n_samples > 1
+    for d in dims:
+        # SizeC can represent RGB (Samples) data rather
+        # than channel data, whether or not this is the case depends
+        # on what the SamplesPerPixel are for the channel
+        if d == "C" and has_multiple_samples:
+            count = len(ome.images[scene_index].pixels.channels)
+        elif d == "S" and has_multiple_samples:
+            count = n_samples
+        else:
+            count = getattr(ome.images[scene_index].pixels, f"size_{d.lower()}")
+        ome_shape.append(count)
+
+    # The file may not have all the data but OME requires certain dimensions
+    # expand to fill
+    expand_dim_ops: list[Optional[slice]] = []
+    for d_size in ome_shape:
+        # Add empty dimension where OME requires dimension but no data exists
+        if d_size == 1:
+            expand_dim_ops.append(None)
+        # Add full slice where data exists
+        else:
+            expand_dim_ops.append(slice(None, None, None))
+
+    # Apply operators to dask array
+    return image_data[tuple(expand_dim_ops)]
+
+
+def _general_data_array_constructor(
+    image_data: np.ndarray,
+    ome: OME,
+    scene_index: int,
+    dims: list[str],
+):
+    # Expand the image data to match the OME empty dimensions
+    image_data = _expand_dims_to_match_ome(
+        image_data=image_data,
+        ome=ome,
+        dims=dims,
+        scene_index=scene_index,
+    )
+
+    # Always order array
+    if DimensionNames.Samples in dims:
+        out_order = DEFAULT_DIMENSION_ORDER_WITH_SAMPLES
+    else:
+        out_order = DEFAULT_DIMENSION_ORDER
+
+    # Transform into order
+    image_data = reshape_data(
+        image_data,
+        "".join(dims),
+        out_order,
+    )
+
+    # Reset dims after transform
+    dims = [d for d in out_order]
+
+    return image_data, dims
+
+
 def get_data_from_ome_tiff(
     tiff: tifffile.TiffFile, scene_index: int, order: str = "CZYX"
 ):
     """returns data in the order asked"""
     assert tiff.is_ome
     dims = _guess_ome_dim_order(tiff, from_xml(tiff.ome_metadata), scene_index)
-    image_data = tiff.series[scene_index].asarray(squeeze=False)
-    assert image_data.shape[5] == 1, f"{image_data.shape=}: S != 1"
-    image_data = image_data[:, :, :, :, :, 0]
+    image_data, dims = _general_data_array_constructor(
+        tiff.series[scene_index].asarray(),
+        from_xml(tiff.ome_metadata),
+        scene_index,
+        dims,
+    )
+
+    # Special rule:
+    # if T>1 and C=1, transpose
+    if (
+        "C" in order
+        and "T" not in order
+        and image_data.shape[dims.index("T")] > 1
+        and image_data.shape[dims.index("C")] == 1
+    ):
+        x = list(range(image_data.ndim))
+        x[dims.index("T")] = dims.index("C")
+        x[dims.index("C")] = dims.index("T")
+        image_data = image_data.transpose(*tuple(x))
+
     unspecified_dims = set(dims).difference(set(order))
     for dim in unspecified_dims:
-        assert image_data.shape[dims.index(dim)] == 1
+        assert (
+            image_data.shape[dims.index(dim)] == 1
+        ), f"the dimension {dim} is not 1 but {image_data.shape[dims.index(dim)]} "
+        " in image of shape {image_data.shape}"
     image_data = image_data.squeeze(
         axis=tuple([dims.index(dim) for dim in unspecified_dims])
     )
