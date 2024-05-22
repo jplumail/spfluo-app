739cc691f2be8da731240d037fda1e14d09b8f5e
Jean Plumail <jplumail@unistra.fr>
2024-05-22 17:32:40 +0200

    fix app run

--- src/spfluo_app/__main__.py
+++ src/spfluo_app/__main__.py
@@ -1,18 +1,16 @@
 import os
 import sys
 
+os.environ["SCIPION_HOME"] = os.path.expanduser(os.path.join("~","scipion"))
+os.environ["SCIPION_USER_DATA"] = os.path.expanduser(os.path.join("~","ScipionFluoUserData"))
 
-def main():
-    os.environ["SCIPION_HOME"] = os.path.expanduser(os.path.join("~","scipion"))
-    os.environ["SCIPION_USER_DATA"] = os.path.expanduser(os.path.join("~","ScipionFluoUserData"))
+if not os.path.exists(os.environ["SCIPION_HOME"]):
+    os.mkdir(os.environ["SCIPION_HOME"])
 
-    if not os.path.exists(os.environ["SCIPION_HOME"]):
-        os.mkdir(os.environ["SCIPION_HOME"])
-    
-    # Necessary when distributing on Mac/Linux
-    # https://gregoryszorc.com/docs/python-build-standalone/main/quirks.html#tcl-tk-support-files
-    os.environ["TCL_LIBRARY"] = os.path.join(os.path.dirname(sys.executable), "..", "lib", "tcl8.6")
+# Necessary when distributing on Mac/Linux
+# https://gregoryszorc.com/docs/python-build-standalone/main/quirks.html#tcl-tk-support-files
+os.environ["TCL_LIBRARY"] = os.path.join(os.path.dirname(sys.executable), "..", "lib", "tcl8.6")
 
-    from scipion.__main__ import main as scipion_main
+from scipion.__main__ import main as scipion_main
 
-    scipion_main()
+scipion_main()
