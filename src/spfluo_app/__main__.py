8c303cff9643fd7affe45fe57e02ee0a88e8ab85
Jean Plumail <jplumail@unistra.fr>
2024-05-23 08:42:20 +0200

    fix pyapp vispy problem with uv enabled and make entry point work

--- src/spfluo_app/__main__.py
+++ src/spfluo_app/__main__.py
@@ -2,16 +2,22 @@ import os
 import sys
 
 
-os.environ["SCIPION_HOME"] = os.path.expanduser(os.path.join("~","scipion"))
-os.environ["SCIPION_USER_DATA"] = os.path.expanduser(os.path.join("~","ScipionFluoUserData"))
+def main():
 
-if not os.path.exists(os.environ["SCIPION_HOME"]):
-    os.mkdir(os.environ["SCIPION_HOME"])
+    os.environ["SCIPION_HOME"] = os.path.expanduser(os.path.join("~","scipion"))
+    os.environ["SCIPION_USER_DATA"] = os.path.expanduser(os.path.join("~","ScipionFluoUserData"))
 
-# Necessary when distributing on Mac/Linux
-# https://gregoryszorc.com/docs/python-build-standalone/main/quirks.html#tcl-tk-support-files
-os.environ["TCL_LIBRARY"] = os.path.join(os.path.dirname(sys.executable), "..", "lib", "tcl8.6")
+    if not os.path.exists(os.environ["SCIPION_HOME"]):
+        os.mkdir(os.environ["SCIPION_HOME"])
 
-from scipion.__main__ import main as scipion_main
+    # Necessary when distributing on Mac/Linux
+    # https://gregoryszorc.com/docs/python-build-standalone/main/quirks.html#tcl-tk-support-files
+    os.environ["TCL_LIBRARY"] = os.path.expanduser("~/.cache/pyapp/distributions/_7904589091198436804/python/lib/tcl8.6")
 
-scipion_main()
+    from scipion.__main__ import main as scipion_main
+
+    scipion_main()
+
+
+if __name__ == "__main__":
+    main()
