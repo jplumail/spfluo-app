04003bc64f0d748a972717b8e1385087df684e49
jplumail <jeanplumail76@gmail.com>
2024-06-04 18:15:53 +0200

    fix main

--- src/spfluo_app/__main__.py
+++ src/spfluo_app/__main__.py
@@ -12,7 +12,7 @@ def main():
 
     # Necessary when distributing on Mac/Linux
     # https://gregoryszorc.com/docs/python-build-standalone/main/quirks.html#tcl-tk-support-files
-    os.environ["TCL_LIBRARY"] = os.path.expanduser("~/.cache/pyapp/distributions/_7904589091198436804/python/lib/tcl8.6")
+    # os.environ["TCL_LIBRARY"] = os.path.expanduser("~/.cache/pyapp/distributions/_7904589091198436804/python/lib/tcl8.6")
 
     from scipion.__main__ import main as scipion_main
 
