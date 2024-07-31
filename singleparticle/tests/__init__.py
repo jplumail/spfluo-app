f95eb366bffe9b89ee4e8049cdcc9dc045b271c6
jplumail <jeanplumail76@gmail.com>
2024-07-31 17:54:48 +0200

    move DataSet to pwfluo

--- singleparticle/tests/__init__.py
+++ singleparticle/tests/__init__.py
@@ -1,10 +1,3 @@
-from pyworkflow.tests import DataSet
+from pwfluo.tests import DataSet
 
-DataSet(
-    name="fluo",
-    folder="fluo-data",
-    files={
-        "isotropic-particles-dir": "generated/isotropic-1.0/particles",
-        "isotropic-psf": "generated/isotropic-1.0/psf.tiff",
-    },
-)
+__all__ = ["DataSet"]
