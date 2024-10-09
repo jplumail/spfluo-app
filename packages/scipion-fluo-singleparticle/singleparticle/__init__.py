d5752b35d894e31af01ba272065f83267ba4c85f
Jean Plumail <jplumail@unistra.fr>
2024-10-09 11:39:52 +0200

    adds paper references

--- packages/scipion-fluo-singleparticle/singleparticle/__init__.py
+++ packages/scipion-fluo-singleparticle/singleparticle/__init__.py
@@ -34,7 +34,7 @@ from singleparticle.constants import (
 from singleparticle.web.server import app
 
 _logo = "icon.png"
-_references = ["Fortun2017"]
+_references = ["7389457", "10105860"]
 
 
 class Config(pw.Config):
